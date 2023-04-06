2929075dbd91f3bc352173c07b4ebdff0dfcbcc0
K K <koke0415@naver.com>
2023-04-06 16:30:08 +0900

    0406 평가 todolist

---
+++ todolist/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TodoApp from './components/TodoApp';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='contests'>
+          <h1 className='title'>Todo - list</h1>
+          <TodoApp/>
+      </div>
+  
+
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/components/TodoApp.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react"
+
+import './TodoApp.css'
+import AllTodos from "./AllTodos"
+import TodayTodos from "./TodayTodos"
+
+const TodoApp = () => {
+
+    const [todos, setTodos] = useState([
+        {id : 1, name : "할 일 1", date : "4월5일"} , 
+        {id : 2, name : "할 일 2", date : "4월5일"} ,
+        {id : 3, name : "할 일 3", date : "4월5일"} ,
+    ])
+    
+
+    const [inputName, setInputName] = useState("");
+
+    let id = todos.length+1;
+
+
+    
+    // 메소드 작성
+    const inputChange = (e)=>{setInputName(e.target.value)}
+
+    const addTodo = ()=>{
+        const newTodos = todos.concat(
+            {
+                id : id++, name : inputName, date : todayDate()
+            }
+        )
+        setTodos(newTodos)
+        setInputName("");
+    }
+
+    const deleteTodo = (id) => {
+        const newTodos = todos.filter(
+            (todo) => todo.id !== id
+        )
+        setTodos(newTodos)
+    }
+
+    const todayDate = () => {
+        const now = new Date();
+        const todayMonth = now.getMonth()+1;
+        const todayDate = now.getDate();
+
+        return todayMonth+"월" + todayDate+"일"
+    }
+
+    const [btnState,setBtnState] = useState("all")
+
+    const allTodos =()=> {
+        setBtnState("all")
+    }
+
+    const todayTodos =()=> {
+        setBtnState("today")
+    }
+
+    const todolist = btnState==="all" ? todos : todos.filter(todo=>todo.date=="4월6일")
+
+
+
+
+    return(
+        <div>
+            <input type="text"
+                onChange={inputChange}
+                value={inputName}
+                placeholder="할 일을 입력하세요"
+            />
+            <button
+                className="inputBtn"
+                onClick={addTodo}
+            >+</button>
+            <hr />
+        
+            
+                
+            <div>
+                <button
+                    onClick={allTodos}
+                    className="btn1"
+                >모든 할 일</button>
+            
+                <button
+                    onClick={todayTodos}
+                    className="btn2"
+                >오늘 할 일</button>
+            </div>
+
+            <ul className="list">
+            {
+                todolist.map((todo)=><li 
+                key={todo.id}
+                className={todo.checked ? "on" : ""}
+                >
+                    
+                    <p>{todo.date}</p>
+                    <input type="checkbox" 
+                        className="checkbox"
+                        checked={(todo.checked)}
+                        readOnly
+                        onClick={()=>{
+                            
+                            const newTodos =  todos.map((t)=>{
+                                if(todo.id !== t.id) {
+                                    return t;
+                                }
+                                else {
+                                    return{...t, checked: !t.checked}
+                                }
+                            })
+                            setTodos(newTodos);
+                        }}
+                    />
+                    {todo.name}
+                    
+                    <button
+                        className="xBtn"
+                        onClick={()=>{deleteTodo(todo.id)}}
+                    >X
+                    </button>
+                </li>)
+            }
+            </ul>
+
+            
+        </div>
+    )
+}
+
+export default TodoApp
